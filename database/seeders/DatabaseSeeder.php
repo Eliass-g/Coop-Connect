b91a78b931a883af39c01f1e06deb5849856e160
Eliass <eliass.ghauss@gmail.com>
2024-06-12 20:09:04 -0400

    setup base structure for database, add controller, model, table, seeder, resource, request, filter file for tokens

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        DB::table('tokens')->insert([
+            'email' => "test@test.ca",
+            'token' => Str::random(10),
+
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Http\Controllers\Api\V1\TokenController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MessagingController;
+use App\Http\Controllers\ConversationController;
+use App\Http\Controllers\DocumentsController;
+use App\Http\Controllers\JobController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/filterjobs', [JobController::class, 'filterJobs']);
+
+Route::post('/sendmessage', [MessagingController::class, 'sendMessage']);
+Route::get('/getmessages', [MessagingController::class, 'fetchMessages']);
+Route::post('/createconversation', [ConversationController::class, 'createConversation']);
+Route::get('/fetchconversationid', [ConversationController::class, 'fetchConversationId']);
+
+Route::post('/uploaddocs', [DocumentsController::class, 'upload']);
+Route::get('/fetchdocs', [DocumentsController::class, 'fetchDoc']);
+Route::delete("/deletedoc/{doc_id}", [DocumentsController::class, "deleteDoc"]);
+
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
+    Route::apiResource('token', TokenController::class);
+});
