65fda3a3bfd3e1cf4f9c619c17db4fe60ff7df6d
Eliass Ghauss <eliass.ghauss@gmail.com>
2024-06-20 17:37:10 -0400

    added redux statemanagement to reflections page

--- resources/js/Pages/Student/Reflections.jsx
+++ resources/js/Pages/Student/Reflections.jsx
@@ -1,8 +1,108 @@
 import * as React from "react";
 import styled from "styled-components";
 import NavBar from "./Components/NavBar";
+<<<<<<< HEAD
 
 function Reflections() {
+=======
+import { useSelector, useDispatch } from "react-redux";
+import {
+    postReflection,
+    selectReflectionsStatus,
+    selectReflections,
+} from "@/Features/reflections/reflectionsSlice";
+
+const badWords = [
+    "bitch",
+    "nigger",
+    "fuck",
+    "cock",
+    "pussy",
+    "shit",
+    "asshole",
+    "dick",
+    "whore",
+    "prick",
+    "bullshit",
+    "bastard",
+];
+
+function Reflections() {
+    const [userId, setUserId] = useState(null);
+    const [reflectionContent, setReflectionContent] = useState("");
+    const [error, setError] = React.useState(null);
+    const [successMessage, setSuccessMessage] = useState("");
+
+    useEffect(() => {
+        // Fetch the XSRF token from cookies and set it in Axios headers
+        const csrfToken = document.cookie
+            .split("; ")
+            .find((row) => row.startsWith("XSRF-TOKEN="))
+            ?.split("=")[1];
+        axios.defaults.headers.common["X-XSRF-TOKEN"] = csrfToken;
+
+        // Fetch the user ID
+        const fetchUserId = async () => {
+            try {
+                const response = await axios.get(
+                    `http://127.0.0.1:8000/api/user-id`
+                );
+                setUserId(response.data.user.id);
+                console.log("userinfo", response.data.user.id);
+            } catch (error) {
+                console.error("Error fetching user ID:", error);
+            }
+        };
+
+        fetchUserId();
+    }, []);
+
+    const dispatch = useDispatch();
+
+    const reflections = useSelector(selectReflections);
+    const reflectionsStatus = useSelector(selectReflectionsStatus);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const hasBadWords = badWords.some((word) =>
+            reflectionContent.toLowerCase().includes(word)
+        );
+
+        if (hasBadWords) {
+            setError("Please remove inappropriate words from your reflection.");
+            return; // Prevent submission if bad words are found
+        }
+
+        const currentDate = new Date().toISOString();
+
+        dispatch(
+            postReflection({
+                userId: userId,
+                content: reflectionContent,
+                jobsId: 4,
+                createdAt: currentDate,
+                updatedAt: currentDate,
+            })
+        )
+            .then((response) => {
+                setSuccessMessage("Reflection posted successfully!");
+                setReflectionContent("");
+                setError(null);
+
+                setTimeout(() => {
+                    setSuccessMessage("");
+                }, 5000);
+            })
+            .catch((error) => {
+                console.error("Error posting reflection:", error);
+                setError(
+                    "There was an error posting your reflection. Please try again."
+                );
+            });
+    };
+
+>>>>>>> df4e85a (added redux statemanagement to reflections page)
     return (
         <NavBar header={"Reflections Upload"}>
             <Section className="forum-container">
@@ -16,7 +116,11 @@ function Reflections() {
                             employers!
                         </Description>
                         <FormContainer>
+<<<<<<< HEAD
                             <Form>
+=======
+                            <Form onSubmit={handleSubmit}>
+>>>>>>> df4e85a (added redux statemanagement to reflections page)
                                 <Label
                                     htmlFor="reflectionInput"
                                     className="reflection-label"
@@ -30,17 +134,42 @@ function Reflections() {
                                     <Textarea
                                         id="reflectionInput"
                                         className="reflection-textarea"
+<<<<<<< HEAD
                                         defaultValue="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
                                     />
+=======
+                                        value={reflectionContent}
+                                        onChange={(e) => {
+                                            setReflectionContent(
+                                                e.target.value
+                                            );
+                                            // Clear error message on input change
+                                            setError(null);
+                                        }}
+                                    />
+
+>>>>>>> df4e85a (added redux statemanagement to reflections page)
                                     <Image
                                         loading="lazy"
                                         src="https://cdn.builder.io/api/v1/image/assets/TEMP/cdd149d58712b078a844655053653df9d81013ce41c2123353ace40a672efdc3?apiKey=d66532d056b14640a799069157705b77&"
                                         alt=""
                                     />
                                 </TextareaWrapper>
+<<<<<<< HEAD
                                 <Button type="submit" className="submit-button">
                                     Post Comment
                                 </Button>
+=======
+                                {error && <ErrorMessage>{error}</ErrorMessage>}
+                                <Button type="submit" className="submit-button">
+                                    Post Comment
+                                </Button>
+                                {successMessage && (
+                                    <SuccessMessage>
+                                        {successMessage}
+                                    </SuccessMessage>
+                                )}
+>>>>>>> df4e85a (added redux statemanagement to reflections page)
                             </Form>
                         </FormContainer>
                     </FormWrapper>
@@ -179,4 +308,19 @@ const Button = styled.button`
     }
 `;
 
+<<<<<<< HEAD
+=======
+const ErrorMessage = styled.p`
+    color: red;
+    font-size: 14px;
+    margin-top: 5px;
+`;
+
+const SuccessMessage = styled.p`
+    color: green;
+    font-weight: bold;
+    margin-top: 10px;
+`;
+
+>>>>>>> df4e85a (added redux statemanagement to reflections page)
 export default Reflections;
